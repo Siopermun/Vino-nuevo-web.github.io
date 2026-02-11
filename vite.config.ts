909ff930d98f200c5d1bf75c4a2693e03590df33
Siopermun <165212535+Siopermun@users.noreply.github.com>
2026-02-11 01:41:27 +0000

    Despliegue final

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()], base: '/Vino-nuevo-web.github.io/', 
+  base: '/vino-nuevo-web.github.io/',
+  // ... el resto de tu configuración
 })
